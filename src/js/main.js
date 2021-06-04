ec38313a1541a8c93f8e4c0903a5f39993e276a2
Jovana <wanatodorowic@gmail.com>
2021-06-04 02:11:27 +0200

    adding landing page styles and js functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
 import "../scss/main.scss";
+
+console.log("hello");
+var navLinkItems = document.querySelectorAll(".nav-link");
+
+navLinkItems.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    navLinkItems.forEach((link) => link.classList.remove("clicked-hr"));
+    navLink.classList.add("clicked-hr");
+  });
+});
+var navbar = document.querySelector(".navbar");
+window.addEventListener("scroll", () => {
+  if (
+    document.body.scrollTop >= 280 ||
+    document.documentElement.scrollTop >= 280
+  ) {
+    navbar.classList.add("scroll");
+  } else {
+    navbar.classList.remove("scroll");
+  }
+});
